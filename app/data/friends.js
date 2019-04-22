03c5ad30c6c462bcad16e354bdc2a8da90210740
Anjana Hengaju <an.hengaju@gmail.com>
2019-04-22 18:57:35 -0500

    HTML page and survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,3 +9,20 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(PORT, function() {
   console.log("connected");
 });
+
+let friends = {
+  name: "Ahmed",
+  photo:
+    "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+};
+
+// HTML routes
+
+app.get("/survey", function(request, respose) {
+  respose.sendfile(path.join(__dirname, "../public/survey.html"));
+});
+
+app.get("/home", function(request, respose) {
+  respose.sendfile(path.join(__dirname, "../public/home.html"));
+});

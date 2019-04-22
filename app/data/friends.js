6e0af84ecc523d39468e42f7c1fa1f487e9b1ab3
Anjana Hengaju <an.hengaju@gmail.com>
2019-04-21 19:14:14 -0500

    initial files

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, function() {
+  console.log("connected");
+});

ffcae4fa28d0c112d1da5dc81bf7a30d0e547728
Anjana Hengaju <an.hengaju@gmail.com>
2019-04-24 23:08:31 -0500

    Refractor all files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,3 @@
-// var express = require("express");
-// var path = require("path");
-
-// var app = express();
-// var PORT = process.env.PORT || 3000;
-
-// app.use(express.urlencoded({ extended: true }));
-// // app.use(express.json());
-
-// app.listen(PORT, function() {
-//   console.log("connected: " + PORT);
-// });
-
 let friends = [
   {
     name: "Ahmed",
@@ -33,44 +20,3 @@ let friends = [
 //export freinds
 
 module.exports = friends;
-// HTML routes
-
-// app.get("/survey", function(request, respose) {
-//   respose.sendFile(path.join(__dirname, "../public/survey.html"));
-// });
-
-// app.get("/home", function(request, respose) {
-//   respose.sendFile(path.join(__dirname, "../public/home.html"));
-// });
-
-// // API routes
-// app.get("/api/friends", function(request, respose) {
-//   respose.json(friends);
-// });
-
-// app.post("/api/friends", function(request, respose) {
-//   const friend = request.body;
-//   const bestFriend = findBestFriend(friends, friend);
-//   friends.push(friend);
-//   respose.json(bestFriend);
-// });
-
-// //find the best match
-// function findBestFriend(friends, friend) {
-//   friends.sort(function(f1, f2) {
-//     const f1ScoreDiff = compareScore(f1.scores, friend.scores);
-//     const f2ScoreDiff = compareScore(f2.scores, friend.scores);
-//     return f1ScoreDiff - f2ScoreDiff;
-//   });
-
-//   return friends[0];
-// }
-// //  Compare scores
-// function compareScore(score1, score2) {
-//   let total = 0;
-//   score1.forEach((score, i) => {
-//     let diff = Math.abs(score1[i] - score2[i]);
-//     total = total + diff;
-//   });
-//   return total;
-// }
